fde991d87f37a6cfb15f381512656333972784c0
Alexandra Holstensson <ro24nz8c@students.codeinstitute.net>
2025-01-28 10:51:13 +0000

    Add new index function to hello_world app

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("Hello, World!")
+
 def index(request):
-    return HttpResponse("Hello, World!")
+   if request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+   elif request.method == "GET":
+       return HttpResponse(request.method)
